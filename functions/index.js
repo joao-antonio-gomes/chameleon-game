83baf70ccb576067a3a204e390ef6d89329b40bd
João Gomes <devjoaoantoniogomes@gmail.com>
2024-10-09 15:47:28 -0300

    add chat gpt integration

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-const { OpenAI } = require("openai");
+const {OpenAI} = require("openai");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -20,9 +19,8 @@ const { OpenAI } = require("openai");
 // });
 
 // functions/index.js (ou index.ts para TypeScript)
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const fetch = require('node-fetch');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 
@@ -31,11 +29,15 @@ const assistantId = "asst_7Def8M08JcePEukjUnCDjfXl";
 exports.getTheme = functions.https.onCall(async (data, context) => {
   // Autenticação opcional
   if (!context.auth) {
-    throw new functions.https.HttpsError('unauthenticated', 'Usuário não autenticado.');
+    logger.error("Usuário não autenticado.");
+    throw new functions.https.HttpsError("unauthenticated",
+        "Usuário não autenticado.");
   }
 
-  if (!data?.roomId) {
-    throw new functions.https.HttpsError('invalid-argument', 'roomId é obrigatório.');
+  if (!data.roomId) {
+    logger.error("roomId é obrigatório.");
+    throw new functions.https.HttpsError("invalid-argument",
+        "roomId é obrigatório.");
   }
 
   const openai = new OpenAI({
@@ -43,17 +45,19 @@ exports.getTheme = functions.https.onCall(async (data, context) => {
   });
 
   // If thread is not created
-  if (!data?.thread) {
+  if (!data.thread) {
+    logger.info("Thread não encontrada. Criando uma nova thread.");
+
     const stream = await openai.beta.threads.createAndRun(
         {
           assistant_id: assistantId,
           thread: {
             messages: [
-              { role: "user", content: "Gere um tema para mim." },
+              {role: "user", content: "Gere um tema para mim."},
             ],
           },
-          stream: true
-        }
+          stream: true,
+        },
     );
 
     let threadId;
@@ -63,12 +67,17 @@ exports.getTheme = functions.https.onCall(async (data, context) => {
       }
     }
 
+    logger.info(`Thread ${threadId} criada.`);
+
     const threadMessages = await openai.beta.threads.messages.list(
-        threadId
+        threadId,
     );
 
-    const lastMessageThemeContent = threadMessages.data[0].content[0].text.value;
+    const lastMessageThemeContent =
+        threadMessages.data[0].content[0].text.value;
     const lastMessageThemeJson = JSON.parse(lastMessageThemeContent);
+
+    logger.info(`Tema gerado: ${lastMessageThemeJson.theme}`);
 
     return {
       thread: threadId,
@@ -78,25 +87,35 @@ exports.getTheme = functions.https.onCall(async (data, context) => {
   }
 
   // If thread is already
+  logger.info(`Thread ${data.thread} encontrada. Enviando mensagem.`);
+
   await openai.beta.threads.messages.create(
       data.thread,
-      { role: "user", content: "Gere um tema para mim." }
+      {role: "user", content: "Gere um tema para mim."},
   );
+
+  logger.info(`Mensagem enviada para thread ${data.thread}.`);
 
   const run = await openai.beta.threads.runs.stream(data.thread, {
     assistant_id: assistantId,
   });
 
+  logger.info(`Thread ${data.thread} em execução.`);
+
   for await (const event of run) {
     if (event.event === "thread.run.completed") break;
   }
 
+  logger.info(`Thread ${data.thread} finalizada.`);
+
   const threadMessages = await openai.beta.threads.messages.list(
-      data.thread
+      data.thread,
   );
 
   const lastMessageThemeContent = threadMessages.data[0].content[0].text.value;
   const lastMessageThemeJson = JSON.parse(lastMessageThemeContent);
+
+  logger.info(`Tema gerado: ${lastMessageThemeJson.theme}`);
 
   return {
     thread: data.thread,
